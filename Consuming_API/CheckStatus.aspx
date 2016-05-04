8415e2462cd02932c053fb0974f07ad00770434b
TEJA1122 <TEJA_1122@yahoo.com>
2016-05-04 12:11:45 +1000

    CheckStatus of NCC done

---
+++ Consuming_API/CheckStatus.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CheckStatus.aspx.cs" Inherits="Consuming_API.CheckStatus" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Enter Check ID:&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Check Status" Width="241px" />
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="Label3" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="Label4" runat="server"></asp:Label>
+        <br />
+        <asp:Label ID="Label5" runat="server"></asp:Label>
+        <br />
+        <asp:HyperLink ID="HyperLink1" runat="server" Visible="False">Crim Check link</asp:HyperLink>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Consuming_API/CheckStatus.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Consuming_API
+{
+    public partial class CheckStatus : System.Web.UI.Page
+    {
+        private const string BASE_ENDPOINT = "https://api-sandbox.nationalcrimecheck.com.au/v1.0/";
+
+        public class RootObject
+        {
+            public int id { get; set; }
+            public string continue_url { get; set; }
+            public string status { get; set; }
+            public string verify_status { get; set; }            
+            public string type { get; set; }
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            //string Key = "y8TnfNQGrj4VQZymZf9J0IlsLijo6Kgu";
+            //string Secret = "IcqHhocypOFJ5ZuWiNbmubyr6nMz6CPE";
+
+            string keyText = "y8TnfNQGrj4VQZymZf9J0IlsLijo6Kgu:IcqHhocypOFJ5ZuWiNbmubyr6nMz6CPE";
+
+            var keyTextBytes = System.Text.Encoding.UTF8.GetBytes(keyText);
+            string base64Encode = System.Convert.ToBase64String(keyTextBytes);
+
+            string base64keyText = base64Encode;
+
+            string authorizationKey = "Basic " + base64keyText;  // End of Key converstion 
+            string create_baseUrl = BASE_ENDPOINT + "checks/" + TextBox1.Text;
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(create_baseUrl);
+
+            request.Headers.Add("Authorization", authorizationKey);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //request.ContentLength = DATA.Length;
+            //StreamWriter requestWriter = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.ASCII); // for post
+            //requestWriter.Write(DATA);
+            //requestWriter.Close();
+
+
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                Stream webStream = webResponse.GetResponseStream();
+                StreamReader responseReader = new StreamReader(webStream);
+                string response = responseReader.ReadToEnd();
+                Console.WriteLine(response);
+
+                RootObject jsonresponse = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<RootObject>(response);
+                int resid = Convert.ToInt32(jsonresponse.id.ToString());
+                Label2.Text = "Client ID is :  " + resid.ToString();
+                Label3.Text = "Check Status :  " + jsonresponse.status.ToString();
+                Label4.Text = "Verify Status:  " + jsonresponse.verify_status.ToString();
+
+                //try
+                //{
+                //    Label5.Text = jsonresponse.type.ToString();
+                //}
+                //catch (Exception ee)
+                //{
+                //    Console.WriteLine("Error Message: ");
+                //    Console.WriteLine(ee.Message);
+                //}
+                HyperLink1.Visible = true;
+                HyperLink1.NavigateUrl = jsonresponse.continue_url.ToString();
+
+                responseReader.Close();
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("Error Message: ");
+                Console.WriteLine(ee.Message);
+            }
+        }
+    }
+}
